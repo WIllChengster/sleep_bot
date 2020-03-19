4762a3a28dbaab591a937aeb2cc8ca958bcad623
William Cheng <willchengsters@gmail.com>
2020-03-18 22:14:30 -0700

    got milliseconds asleep to print

--- app.js
+++ app.js
@@ -17,33 +17,37 @@ client.on('message', msg => {
     //
     //if author of msg was already sleeping, unmark --> send msg that "bro woke up"
     //
+    let asleepUser;
     for (let i = 0; i < sleepers.length; i++) {
-        if (sleepers[i] === msg.author.id){
+        if (sleepers[i][msg.author.id]){
             authorIsSleeping = true;
+            asleepUser = sleepers[i]
             sleepers.splice(i, 1);
         }
     }
 
     
     if (authorIsSleeping) {
-
-        msg.reply(`<@!${msg.author.id}> woke up from their long slumber`);
-
+        const currDate = new Date();
+        msg.reply(`<@!${msg.author.id}> woke up from their long slumber. This user has been sleeping for ${currDate - Object.values(asleepUser)[0]} milliseconds `);
 
     } else if (msg.content.startsWith('sleep')) {
         const user = msg.mentions.users.first()
         //if user exists...
         if (!!user) {
-
             let userIsSleeping = false;
             for (let i = 0; i < sleepers.length; i++) {
-                if (sleepers[i] === user.id)
+                if (sleepers[i][user.id])
                     userIsSleeping = true;
             }
             if (userIsSleeping) {
                 msg.reply(`User: <@!${user.id}> has already been marked as sleeping`)
             } else {
-                sleepers.push(user.id.toString())
+                //pushes an object to sleepers Arr in this format
+                // {userid: dateObj}
+                sleepers.push({
+                    [user.id.toString()]: new Date(),
+                })
                 msg.reply(`User: <@!${user.id}> is now marked as sleeping`);
             }
 
